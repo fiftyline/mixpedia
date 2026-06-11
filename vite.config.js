0410b92930f2cfd0b832a2a44ea9f76ca54093c7
Chan-Sol Lee <coldkid1124@gmail.com>
2026-06-11 15:52:06 +0900

    260611

--- vite.config.js
+++ vite.config.js
@@ -9,7 +9,7 @@ export default defineConfig({
     strictPort: true,
     proxy: {
       "/api": {
-        target: "http://127.0.0.1:8001",
+        target: "http://127.0.0.1:8001", 
         // target: "https://mixpedia-fastapi-50799312352.asia-northeast3.run.app",
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, ""),
